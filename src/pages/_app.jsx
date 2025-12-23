56e9fed108123103ef3a597a71d18669d5bf7213
ejhover <emmethoversten@gmail.com>
2025-12-23 14:40:27 -0600

    favicon and title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
 import React from 'react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Emmet Hoversten | Software Engineer</title>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
